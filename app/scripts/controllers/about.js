4d8bd1e0bf95a6896b4422c2a775dc6272290d50
Harshal Shah <harshalshah@HS27.local>
2016-01-30 15:31:56 +1100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lunchboxApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lunchboxApp
+ */
+angular.module('lunchboxApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lunchboxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lunchboxApp
+ */
+angular.module('lunchboxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
