33aba7020dca2a5ca9895f8eef0ecdd57a791a7e
jeyjk <jk.jeyakumaran@saiglobal.com>
2016-02-23 09:17:54 +1100

    adding the cloud count

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,28 +1,31 @@
 'use strict';
 var lunchboxApp = angular.module('lunchboxApp');
-lunchboxApp.controller('LunchesCtrl', ['$scope', '$location', 'Lunchers', function ($scope, $location, Lunchers) {  
-   $scope.searchText = $location.search().searchText;
-   $scope.lunchers = Lunchers.query();   
-}]);
-
-lunchboxApp.controller('CloudCtrl', ['$scope', '$q', 'Tags', function ($scope, $q, Tags) {  
-    $scope.tags = Tags.all() || [];
-    $scope.words = []; 
-    $scope.tags.success(function (data) {        
-        for (var i = 0; i <  data.length; i++) {
-            var tag =   data[i];    
-            $scope.words.push({
-                text : tag,
-                weight: 3,
-                link: '#//main?searchText=' + tag,
-                handlers : {
-                    click: function() {  
-                        $scope.searchText = tag; 
-                    }
+lunchboxApp.controller('LunchesCtrl', ['$scope', '$location', 'filterFilter', 'Lunchers', function ($scope, $location, filterFilter, Lunchers) {  
+    $scope.searchText = $location.search().searchText;
+    $scope.lunchers = [];
+    $scope.words = [];
+    Lunchers.all().then( function(result) {
+        $scope.lunchers = result.data;       
+        for (var i = 0; i <  $scope.lunchers.length; i++) {     
+            var tags =   $scope.lunchers[i].tags;            
+            for (var k = 0; k <  tags.length; k++) {
+                var tag = tags[k];
+                var existingTag = filterFilter($scope.words, { text: tag });
+                if(existingTag && existingTag.length > 0) {                    
+                    existingTag[0].weight = existingTag[0].weight + 1;                    
+                } else {
+                    $scope.words.push({
+                        text : tag,
+                        weight: 0,
+                        link: '#//main?searchText=' + tag,
+                        handlers : {
+                            click: function() {  
+                                $scope.searchText = tag; 
+                            }
+                        }
+                    });
                 }
-            });          
-        }
-    });
+            }
+        }       
+   });    
 }]);
-
-
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,10 +1,10 @@
 'use strict';
-var services = angular.module('services', ['ngResource']);
-services.factory('Lunchers', ['$resource', function($resource) { 
-    return $resource('store/lunchers.json', {}, {
-        query: {method:'GET', params:{luncher:'lunchers'}, isArray:true}
-    });  
-}]);
+var services = angular.module('services', []);
+services.service('Lunchers', function($http) { 
+    this.all = function () {
+        return $http.get('store/lunchers.json');
+    };  
+});
 services.service('Tags', function($http) { 
     this.all = function () {
         return $http.get('store/tags.json');
